c8ad453fd2a58179973e9999c905265688574d09
Wallacy Pasqualini <98925497+WallPasq@users.noreply.github.com>
2022-12-11 17:41:03 -0300

    JavaScript function to find range of prime numbers
    I started by training in JavaScript and creating a function to find a range of prime numbers between two numbers. Then I will create HTML and CSS files to develop a website to show a series of prime numbers according to user interaction.

---
+++ primeRangeWebsite/primeRange.js
@@ -0,0 +1,25 @@
+function primeRange(initial_number, final_number) {
+    let primes = [];
+    let is_prime = true;
+    let limit = 0;
+    for (let n = 2; n < (final_number + 1); n++) {
+        limit = Math.ceil(n / 2);
+        for (let i = 0; i < primes.length; i++) {
+            if (primes[i] > limit) {
+                break;
+            }
+            else if (n % primes[i] == 0) {
+                is_prime = false;
+                break;
+            }
+        }
+        if (is_prime) {
+            primes.push(n);
+        }
+        is_prime = true;
+    }
+    primes = primes.filter(prime => prime >= initial_number);
+    return primes;
+}
+
+console.log(primeRange(2, 100));
