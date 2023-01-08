b3b3e5c1ccb869cce1994f1a74a63a7ec2f951d5
WallPasq <wallacypasqualini@gmail.com>
2023-01-08 20:43:11 -0300

    Accounting for generated prime numbers

--- primeRangeWebsite/js/generator.js
+++ primeRangeWebsite/js/generator.js
@@ -1,25 +1,27 @@
 function primeRange() {
     let primes = [];
     let is_prime = true;
-    let limit = 0;
-    let final_number = Number(document.getElementById("finalNumber").value);
+    let limit = 0, c = 0, p = 2;
+    let count = Number(document.getElementById("count").value);
 
-    for (let n=2; n<(final_number+1); n++) {
-        limit = Math.ceil(Math.sqrt(n));
+    while (c<count) {
+        limit = Math.ceil(Math.sqrt(p));
 
         for (let i=0; i<primes.length; i++) {
             if (primes[i] > limit) {
                 break;
             }
-            else if (n % primes[i] == 0) {
+            else if (p % primes[i] == 0) {
                 is_prime = false;
                 break;
             }
         }
         if (is_prime) {
-            primes.push(n);
+            primes.push(p);
+            c++;
         }
         is_prime = true;
+        p++;
     }
     document.getElementById('printPrimeRange').innerHTML = primes.join(', ');
 }
